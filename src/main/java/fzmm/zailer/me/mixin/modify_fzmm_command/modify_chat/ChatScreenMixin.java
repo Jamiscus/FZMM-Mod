6472d74b6106ba0a06ec095ccc9028fee8502b50
Zailer43 <minecraftpelusa@gmail.com>
2024-05-04 12:45:11 -0300

    Allows to place special characters inside /fzmm (e.g. §)

--- src/main/java/fzmm/zailer/me/mixin/modify_fzmm_command/modify_chat/ChatScreenMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/modify_fzmm_command/modify_chat/ChatScreenMixin.java
@@ -1,8 +1,9 @@
-package fzmm.zailer.me.mixin.modify_fzmm_command;
+package fzmm.zailer.me.mixin.modify_fzmm_command.modify_chat;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import fzmm.zailer.me.mixin_interfaces.IAllowParagraphs;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,7 +20,7 @@ public abstract class ChatScreenMixin {
 
     @Inject(method = "onChatFieldUpdate", at = @At("HEAD"))
     private void fzmm$onChatFieldUpdate(String chatText, CallbackInfo ci) {
-        this.fzmm$setFzmmCommandMaxLength(chatText);
+        this.fzmm$tryModifyChat(chatText);
     }
 
     @WrapOperation(
@@ -27,18 +28,21 @@ public abstract class ChatScreenMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setText(Ljava/lang/String;)V")
     )
     private void fzmm$setText(TextFieldWidget instance, String text, Operation<Void> original) {
-        this.fzmm$setFzmmCommandMaxLength(text);
+        this.fzmm$tryModifyChat(text);
         original.call(instance, text);
     }
 
     @Unique
-    private void fzmm$setFzmmCommandMaxLength(String message) {
-        if (this.fzmm$isFzmmCommand(message))
+    private void fzmm$tryModifyChat(String message) {
+        if (this.fzmm$isFzmmCommand(message)) {
             this.chatField.setMaxLength(200000);
-        else {
-            if (this.chatField.getCursor() > 256)
+            ((IAllowParagraphs) this.chatField).fzmm$setAllowParagraphs(true);
+        } else {
+            if (this.chatField.getCursor() > 256) {
                 this.chatField.setCursor(Math.min(256, this.chatField.getText().length()), false);
+            }
             this.chatField.setMaxLength(256);
+            ((IAllowParagraphs) this.chatField).fzmm$setAllowParagraphs(false);
         }
     }
 
