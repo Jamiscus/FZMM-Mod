a47701235ed3d5f943c13d94356bbf536477b5e0
Zailer43 <minecraftpelusa@gmail.com>
2024-05-05 00:15:57 -0300

    Fixes merge error in mixins

--- src/main/java/fzmm/zailer/me/mixin/block_state_editor/VerticallyAttachableBlockItemAccessor.java
+++ src/main/java/fzmm/zailer/me/mixin/block_state_editor/VerticallyAttachableBlockItemAccessor.java
@@ -1,10 +1,13 @@
 package fzmm.zailer.me.mixin.block_state_editor;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.item.VerticallyAttachableBlockItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+@Environment(EnvType.CLIENT)
 @Mixin(VerticallyAttachableBlockItem.class)
 public interface VerticallyAttachableBlockItemAccessor {
 
