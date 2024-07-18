a643d6362b4d4e624950c197024bfe0d154919c0
Zailer43 <minecraftpelusa@gmail.com>
2024-07-18 12:06:28 -0300

    Fix incorrect z-index in the suggestion text box of the head generator overlay parameters

--- src/main/java/fzmm/zailer/me/client/gui/components/row/image/ImageRowsElements.java
+++ src/main/java/fzmm/zailer/me/client/gui/components/row/image/ImageRowsElements.java
@@ -3,16 +3,13 @@ package fzmm.zailer.me.client.gui.components.row.image;
 import fzmm.zailer.me.client.gui.components.SuggestionTextBox;
 import fzmm.zailer.me.client.gui.components.image.ImageButtonComponent;
 import fzmm.zailer.me.client.gui.components.image.ImageMode;
-import io.wispforest.owo.config.ui.component.ConfigTextBox;
 import io.wispforest.owo.ui.component.ButtonComponent;
 
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
-@SuppressWarnings("UnstableApiUsage")
 public record ImageRowsElements(ImageButtonComponent imageButton,
-                                ConfigTextBox valueField,
+                                SuggestionTextBox valueField,
                                 AtomicReference<ImageMode> mode,
-                                HashMap<ImageMode, ButtonComponent>  imageModeButtons,
-                                SuggestionTextBox suggestionTextBox) {
+                                HashMap<ImageMode, ButtonComponent>  imageModeButtons) {
 }
