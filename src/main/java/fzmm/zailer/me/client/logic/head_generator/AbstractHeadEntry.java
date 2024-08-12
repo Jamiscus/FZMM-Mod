8b93c740d015bf576ca8ad602f012ee6eddac628
Zailer43 <minecraftpelusa@gmail.com>
2024-08-12 07:34:48 -0300

    Change: in head generator, if the head name ends with a number and there is no space before it, add a space before it

--- src/main/java/fzmm/zailer/me/client/logic/head_generator/AbstractHeadEntry.java
+++ src/main/java/fzmm/zailer/me/client/logic/head_generator/AbstractHeadEntry.java
@@ -25,9 +25,14 @@ public abstract class AbstractHeadEntry {
         String[] folders = path.split("/");
         String fileName = folders.length != 0 ? folders[folders.length - 1] : path;
 
-        String displayName = fileName.replaceAll("_", " ");
-        String firstCharacter = String.valueOf(displayName.charAt(0));
-        return displayName.replaceFirst(firstCharacter, firstCharacter.toUpperCase());
+        String displayName = fileName.substring(0, 1).toUpperCase() + fileName.substring(1);
+
+        // If the fileName ends with a number and not have space before it, add a space before it
+        if (fileName.matches(".*[0-9]+$") && !fileName.matches(".* [0-9]+$")){
+            displayName = displayName.replaceFirst("([0-9]+$)", " $1");
+        }
+
+        return displayName.replaceAll("_", " ");
     }
 
     public Text getDisplayName() {
