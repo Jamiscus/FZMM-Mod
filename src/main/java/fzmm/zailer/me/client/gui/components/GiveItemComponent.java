748401fafc2e2a27458275c47ac68fe8ba329468
Zailer43 <minecraftpelusa@gmail.com>
2024-08-17 13:09:29 -0300

    Fix issue where the tooltip context should not be null * This fixes compatibility with the NBT Tooltip mod

--- src/main/java/fzmm/zailer/me/client/gui/components/GiveItemComponent.java
+++ src/main/java/fzmm/zailer/me/client/gui/components/GiveItemComponent.java
@@ -4,22 +4,14 @@ import fzmm.zailer.me.utils.FzmmUtils;
 import io.wispforest.owo.ui.component.ItemComponent;
 import io.wispforest.owo.ui.core.CursorStyle;
 import io.wispforest.owo.ui.util.UISounds;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.tooltip.TooltipType;
 
 public class GiveItemComponent extends ItemComponent {
     public GiveItemComponent(ItemStack stack) {
         super(stack);
         this.showOverlay(true);
 
-        MinecraftClient client = MinecraftClient.getInstance();
-        this.tooltip(stack.getTooltip(
-                Item.TooltipContext.DEFAULT,
-                client.player,
-                client.options.advancedItemTooltips ? TooltipType.Default.ADVANCED : TooltipType.Default.BASIC
-        ));
+        this.setTooltipFromStack(true);
 
         this.cursorStyle(CursorStyle.HAND);
         this.mouseDown().subscribe((mouseX, mouseY, button) -> {
